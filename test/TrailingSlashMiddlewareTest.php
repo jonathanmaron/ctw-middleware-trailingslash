4ee3ac6a89b5b75950e2af2ee4689073629bb2c1
Jonathan Maron <jonathan.maron@theimagingsource.com>
2021-03-16 10:50:26 +0100

    Improved tests

--- test/TrailingSlashMiddlewareTest.php
+++ test/TrailingSlashMiddlewareTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace CtwTest\Middleware\TrailingSlashMiddleware;
 
 use Ctw\Http\HttpStatus;
+use Laminas\ServiceManager\ServiceManager;
 use Ctw\Middleware\TrailingSlashMiddleware\TrailingSlashMiddleware;
+use Ctw\Middleware\TrailingSlashMiddleware\TrailingSlashMiddlewareFactory;
 use Middlewares\Utils\Dispatcher;
 use Middlewares\Utils\Factory;
 
@@ -12,14 +14,22 @@ class TrailingSlashMiddlewareTest extends AbstractCase
 {
     public function testTrailingSlashMiddleware(): void
     {
-        $request      = Factory::createServerRequest('GET', '/path');
-        $stack        = [
-            new TrailingSlashMiddleware(),
+        $request  = Factory::createServerRequest('GET', '/path');
+        $stack    = [
+            $this->getInstance(),
         ];
-        $response     = Dispatcher::run($stack, $request);
+        $response = Dispatcher::run($stack, $request);
         $this->assertEquals(HttpStatus::STATUS_MOVED_PERMANENTLY, $response->getStatusCode());
-        $headers      = $response->getHeaders();
+        $headers = $response->getHeaders();
         $this->assertTrue(isset($headers['Location'][0]));
         $this->assertEquals('/path/', $headers['Location'][0]);
+    }
+
+    private function getInstance(): TrailingSlashMiddleware
+    {
+        $container = new ServiceManager();
+        $factory   = new TrailingSlashMiddlewareFactory();
+
+        return $factory->__invoke($container);
     }
 }
