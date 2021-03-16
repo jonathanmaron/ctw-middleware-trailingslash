56665591ab11ca39fed09af79f848ba15cda20cc
Jonathan Maron <jonathan.maron@theimagingsource.com>
2021-03-16 15:35:39 +0100

    Improved tests

--- test/TrailingSlashMiddlewareTest.php
+++ test/TrailingSlashMiddlewareTest.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 namespace CtwTest\Middleware\TrailingSlashMiddleware;
 
 use Ctw\Http\HttpStatus;
-use Laminas\ServiceManager\ServiceManager;
 use Ctw\Middleware\TrailingSlashMiddleware\TrailingSlashMiddleware;
 use Ctw\Middleware\TrailingSlashMiddleware\TrailingSlashMiddlewareFactory;
+use Laminas\ServiceManager\ServiceManager;
 use Middlewares\Utils\Dispatcher;
 use Middlewares\Utils\Factory;
 
@@ -21,8 +21,33 @@ class TrailingSlashMiddlewareTest extends AbstractCase
         $response = Dispatcher::run($stack, $request);
         $this->assertEquals(HttpStatus::STATUS_MOVED_PERMANENTLY, $response->getStatusCode());
         $headers = $response->getHeaders();
-        $this->assertTrue(isset($headers['Location'][0]));
+        $this->assertArrayHasKey('Location', $headers,);
+        $this->assertArrayHasKey(0, $headers['Location']);
         $this->assertEquals('/path/', $headers['Location'][0]);
+    }
+
+    public function testTrailingSlashMiddlewareWithNoPath(): void
+    {
+        $request  = Factory::createServerRequest('GET', '');
+        $stack    = [
+            $this->getInstance(),
+        ];
+        $response = Dispatcher::run($stack, $request);
+        $this->assertEquals(HttpStatus::STATUS_MOVED_PERMANENTLY, $response->getStatusCode());
+        $headers = $response->getHeaders();
+        $this->assertArrayHasKey('Location', $headers,);
+        $this->assertArrayHasKey(0, $headers['Location']);
+        $this->assertEquals('/', $headers['Location'][0]);
+    }
+
+    public function testTrailingSlashMiddlewareWithTrailingSlash(): void
+    {
+        $request  = Factory::createServerRequest('GET', '/path/');
+        $stack    = [
+            $this->getInstance(),
+        ];
+        $response = Dispatcher::run($stack, $request);
+        $this->assertEquals(HttpStatus::STATUS_OK, $response->getStatusCode());
     }
 
     private function getInstance(): TrailingSlashMiddleware
