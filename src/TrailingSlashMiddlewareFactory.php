2bbd1f8b5bd9fc66cf4aed3657af5e1c9419a91e
Jonathan Maron <jonathan.maron@theimagingsource.com>
2024-04-19 09:07:51 +0200

    Added factory

--- src/TrailingSlashMiddlewareFactory.php
+++ src/TrailingSlashMiddlewareFactory.php
@@ -9,6 +9,19 @@ class TrailingSlashMiddlewareFactory
 {
     public function __invoke(ContainerInterface $container): TrailingSlashMiddleware
     {
-        return new TrailingSlashMiddleware();
+        $config = [];
+        if ($container->has('config')) {
+            $config = $container->get('config');
+            assert(is_array($config));
+            $config = $config[TrailingSlashMiddleware::class];
+        }
+
+        $middleware = new TrailingSlashMiddleware();
+
+        if ((is_countable($config) ? count($config) : 0) > 0) {
+            $middleware->setConfig($config);
+        }
+
+        return $middleware;
     }
 }
